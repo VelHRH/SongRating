96c6a99ce5878fdf3bd3ead4e3440ea749f97293
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2023-03-23 17:58:11 +0300

    feat: song model

---
+++ SongRating.Core/ISongServices.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace SongRating.Core
+{
+    interface ISongServices
+    {
+        List<Song> GetSongs();
+    }
+}
---
+++ SongRating.WebApi/Controllers/SongsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SongRating.WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SongRating.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SongsController : Controller
+    {
+        public SongsController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult GetSongs()
+        {
+
+        }
+    }
+}
